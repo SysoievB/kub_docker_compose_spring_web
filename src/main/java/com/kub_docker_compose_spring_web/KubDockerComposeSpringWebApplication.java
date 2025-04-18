d5e6f46604288eb38998688e6b57c19eafbe7ea8
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-04-18 13:38:33 +0200

    Init commit

---
+++ src/main/java/com/kub_docker_compose_spring_web/KubDockerComposeSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.kub_docker_compose_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubDockerComposeSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubDockerComposeSpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kub_docker_compose_spring_web/KubDockerComposeSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kub_docker_compose_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubDockerComposeSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
